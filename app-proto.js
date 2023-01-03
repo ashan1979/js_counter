585ec61a4a3b5c3b7dd8a440849146ddeb9d2019
ashan <indrajee79@gmail.com>
2023-01-03 11:09:22 +0530

    js stuff

--- app-proto.js
+++ app-proto.js
@@ -9,5 +9,14 @@ function getElement(selection) {
 }
 
 function Counter (element, value) {
-
+    this.counter = element;
+    this.value = value;
+    this.resetBtn = element.querySelector('.reset');
+    this.decreaseBtn = element.querySelector('.decrease');
+    this.valueDOM = element.querySelector('.value');
+    this.valueDOM.textContent = this.value;
+    // bind this to all functions
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.reset =  this.reset.bind(this);
 }
