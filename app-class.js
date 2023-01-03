79e28f951d1462e840edb5f7a8d297201fbe3091
ashan <indrajee79@gmail.com>
2023-01-03 11:27:51 +0530

    js stuff

--- app-class.js
+++ app-class.js
@@ -16,5 +16,15 @@ class Counter {
         this.resetBtn = element.querySelector('.reset');
         this.increaseBtn = element.querySelector('.increase');
         this.decreaseBtn = element.querySelector('.decrease');
+        this.valueDOM = element.querySelector('.value');
+        this.valueDOM.textContent = this.value;
+        // bind this to all fucntions
+        this.increase =  this.increase.bind(this);
+        this.decrease =  this.decrease.bind(this);
+        this.reset =  this.reset.bind(this);
+
+        this.increaseBtn.addEventListener('click', this.increase);
+        this.decreaseBtn.addEventListener('click', this.decrease);
+        this.resetBtn.addEventListener('click', this.reset);
     }
 }
